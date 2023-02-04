28cc621e368f3efc88db9b2ccbf3100d60045694
BButner <butner.beau@gmail.com>
2023-02-03 20:35:33 -0600

    Adds functionality to select monitors

--- src/lib/fenceState.ts
+++ src/lib/fenceState.ts
@@ -7,7 +7,5 @@ export interface IFenceState {
 export const getState = async (): Promise<IFenceState | null> => {
   const state = await invoke<IFenceState | null>("get_state")
 
-  console.log("state raw", state)
-
   return state
 }
