452461d4eef2b21933d360d6652cfa09a153db4a
BButner <butner.beau@gmail.com>
2023-02-03 23:50:51 -0600

    Fix settings button on dark mode

--- src/components/ConnectedView/SettingsView.tsx
+++ src/components/ConnectedView/SettingsView.tsx
@@ -2,7 +2,7 @@ import { Cog6ToothIcon } from "@heroicons/react/24/outline"
 
 export const SettingsView: React.FC = () => {
   return (
-    <button className="absolute bottom-4 right-4 bg-transparent p-2 text-black hover:animate-spin hover:bg-transparent">
+    <button className="absolute bottom-4 right-4 bg-transparent p-2 text-black hover:animate-spin hover:bg-transparent dark:text-gray-200">
       <Cog6ToothIcon className="h-6 w-6" />
     </button>
   )
